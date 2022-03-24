96c40acc8aeb0e822d7732efbebd4b35267f7d5f
F_Shourabadi <faramarz_sh20@yahoo.com>
2022-03-24 16:35:10 +0430

    update infifnit Scroll component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,11 @@
 import React, { useEffect, useState, useCallback, useRef } from 'react'
-import useFetch from "./components/infiniteScroll/controller/useFetch";
+import { InfinitScroll } from './components/infiniteScroll/view/InfinitScroll';
 
 const App = () => {
-  const [query, setQuery] = useState("");
-  const [page, setPage] = useState(1);
-  const { loading, error, list } = useFetch(query, page);
-  const loader = useRef(null);
-
-  const handleChange = (e) => {
-    setQuery(e.target.value);
-  };
-
-  const handleObserver = useCallback((entries) => {
-    const target = entries[0];
-    if (target.isIntersecting) {
-      setPage((prev) => prev + 1);
-    }
-  }, []);
-
-  useEffect(() => {
-    const option = {
-      root: null,
-      rootMargin: "20px",
-      threshold: 0
-    };
-    const observer = new IntersectionObserver(handleObserver, option);
-    if (loader.current) observer.observe(loader.current);
-  }, [handleObserver]);
 
   return (
     <div className="App">
-      <h1>Infinite Scroll</h1>
-      <input type="text" value={query} onChange={handleChange} />
-      <div>
-        {list.map((item, i) => (
-          <div key={i} style={{ backgroundColor: i % 2 == 0 ? 'gray' : '#0e0e0e0e', padding: "0.3rem", color: i % 2 !== 0 ? 'gray' : '#fefefe' }}>{item.status + ' ' + item.id}</div>
-        ))}
-      </div>
-      {loading && <p style={{ color: 'red', fontSize: '2rem' }}>Loading...</p>}
-      {error && <p>Error!</p>}
-      <div ref={loader} />
+      <InfinitScroll />
     </div>
   );
 }
---
+++ src/components/infiniteScroll/view/InfinitScroll.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState, useCallback, useRef } from 'react'
+import useFetch from "./../controller/useFetch";
+
+function InfinitScroll() {
+    const [query, setQuery] = useState("");
+    const [page, setPage] = useState(1);
+    const { loading, error, list } = useFetch(query, page);
+    const loader = useRef(null);
+
+    const handleChange = (e) => {
+        setQuery(e.target.value);
+    };
+
+    const handleObserver = useCallback((entries) => {
+        const target = entries[0];
+        if (target.isIntersecting) {
+            setPage((prev) => prev + 1);
+        }
+    }, []);
+
+    useEffect(() => {
+        const option = {
+            root: null,
+            rootMargin: "20px",
+            threshold: 0
+        };
+        const observer = new IntersectionObserver(handleObserver, option);
+        if (loader.current) observer.observe(loader.current);
+    }, [handleObserver]);
+
+    return (
+        <div className="App">
+            <h1>Infinite Scroll</h1>
+            <div>
+                {list.map((item, i) => (
+                    <div key={i} style={{ backgroundColor: i % 2 == 0 ? 'gray' : '#0e0e0e0e', padding: "0.3rem", color: i % 2 !== 0 ? 'gray' : '#fefefe' }}>{item.status + ' ' + item.id}</div>
+                ))}
+            </div>
+            {loading && <p style={{ color: 'red', fontSize: '2rem' }}>Loading...</p>}
+            {error && <p>Error!</p>}
+            <div ref={loader} />
+        </div>
+    );
+}
+
+export { InfinitScroll };
